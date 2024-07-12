95956b916bc285c6a2e01bb716e987c0edabfe68
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-07-12 16:06:45 -0700

    added code to create server and first route

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const api = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use('/api', api);
+
+app.listen(PORT, ()=>{
+
+    console.info(`App listening at http://localhost:${PORT}`);
+
+});
